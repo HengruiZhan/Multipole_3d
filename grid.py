edd4b27452b48b905c9ec2e0e86709d11567e902
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-11-01 22:57:09 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -48,9 +48,9 @@ class Grid():
     def __init__(self, nx, ny, nz, xlim, ylim, zlim):
         """a cartisian grid"""
 
-        self.nx = nx-1
-        self.ny = ny-1
-        self.nz = nz-1
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
 
         self.xlim = xlim
         self.ylim = ylim
@@ -60,14 +60,11 @@ class Grid():
         self.dy = (self.ylim[1] - self.ylim[0])/ny
         self.dz = (self.zlim[1] - self.zlim[0])/nz
 
-        self.x = np.linspace(self.xlim[0]+self.dx/2, self.xlim[1]-self.dx/2,
-                             self.nx)
+        self.x = (np.arange(self.nx) + 0.5)*self.dz + self.zlim[0]
 
-        self.y = np.linspace(self.ylim[0]+self.dy/2, self.ylim[1]-self.dy/2,
-                             self.ny)
+        self.y = (np.arange(self.ny) + 0.5)*self.dz + self.zlim[0]
 
-        self.z = np.linspace(self.zlim[0]+self.dz/2, self.zlim[1]-self.dz/2,
-                             self.nz)
+        self.z = (np.arange(self.nz) + 0.5)*self.dz + self.zlim[0]
 
         self.x3d, self.y3d, self.z3d = np.meshgrid(self.x, self.y, self.z,
                                                    indexing='ij')
@@ -87,3 +84,4 @@ class Grid():
 
     def scratch_z_plane_array(self):
         return np.zeros((self.nx, self.ny), dtype=np.float64)
+
