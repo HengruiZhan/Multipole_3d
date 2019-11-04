b8e4207dfc52150cc8770062e23ab3cc1f074014
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-11-03 22:46:12 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as plt
 import multipole
 import numpy as np
 import L2_difference as L2
-# from gekko import GEKKO
+import comparitive_difference as comdf
 
 # test with 16*16*16, 32*32*32 and 64*64*64
+
 nx = 16
 ny = 16
 nz = 16
+
+
+nx = 32
+ny = 32
+nz = 32
 
 xlim = (-1.0, 1.0)
 ylim = (-1.0, 1.0)
@@ -25,9 +31,9 @@ dens[radius <= 0.3] = 1.0
 """
 
 # density of the cube
-a = 0.3
-b = 0.3
-c = 0.3
+a = 0.5
+b = 0.5
+c = 0.5
 sites = (a, b, c)
 # density of a cube
 for i in range(g.nx):
@@ -40,27 +46,24 @@ for i in range(g.nx):
                     g.z[k] >= 0 and \
                     g.z[k] <= c:
                 dens[i, j, k] = 1.0
-'''
+
 # analytical potential of a cube
-V_cube = g.scratch_y_plane_array()
+phi_anal = g.scratch_y_plane_array()
 for i in range(g.nx):
     for j in range(g.nz):
-        cube_V = ap.Analytical_Cube_Potential(sites, g.x[i], g.y[0], g.z[j])
-        V_cube[i, j] = cube_V.V
-        '''
+        cube_V = ap.Analytical_Cube_Potential(sites, g.x[i], g.y[5], g.z[j])
+        phi_anal[i, j] = cube_V.V
 
-n_moments = 0
+lmax = 0
 center = (a/2, b/2, c/2)
 # center = (0.0, 0.0, 0.0)
-moment = multipole.Multipole(g, dens, n_moments, 2*g.dr, center=center)
+m = multipole.Multipole(g, dens, lmax, 0.3*g.dr, center=center)
 
 phi = g.scratch_y_plane_array()
 
-for i in range(g.nx):
-    for j in range(g.nz):
-        # phi[i, j] = moment.phi(g.x[i] - g.dx/2, g.y[0], g.z[j])
-        phi[i, j] = moment.phi(g.x[i], g.y[0], g.z[j], g.dx/2)
+phi = m.PhiY(5)
 
+'''
 plt.imshow(np.log10(np.abs(np.transpose(phi))), origin="lower",
            interpolation="nearest",
            extent=[g.xlim[0], g.xlim[1],
@@ -69,10 +72,38 @@ plt.imshow(np.log10(np.abs(np.transpose(phi))), origin="lower",
 plt.colorbar()
 ax = plt.gca()
 ax.set_aspect("equal")
-plt.savefig("lmax=0.png")
+plt.savefig("lmax=10.png")
 '''
-L2normerr = L2.L2_diff(V_cube, phi, g.scratch_array())
-print("x_off with dx/2")
-print("lmax =", n_moments-1)
+'''
+# test convergence
+# analytical potential of a cube for the whole space
+anal_phi = g.scratch_array()
+for i in range(g.nx):
+    for j in range(g.ny):
+        for k in range(g.nz):
+            cube_V = ap.Analytical_Cube_Potential(sites, g.x[i], g.y[j], g.z[k])
+            anal_phi[i, j, k] = cube_V.V
+
+lmax = 10
+center = (a/2, b/2, c/2)
+# center = (0.0, 0.0, 0.0)
+m = multipole.Multipole(g, dens, lmax, 0.3*g.dr, center=center)
+phi = m.Phi()
+
+# normalized L2 norm error
+L2normerr = L2.L2_diff(anal_phi, phi, g)
+
+print("lmax =", lmax)
 print(L2normerr)
+'''
+'''
+diff = comdf.comp_diff(phi_anal, phi)
+plt.imshow(np.abs(np.transpose(diff.difference)), origin="lower",
+           interpolation="nearest",
+           extent=[g.xlim[0], g.xlim[1],
+                   g.zlim[0], g.zlim[1]])
+plt.colorbar()
+ax = plt.gca()
+ax.set_aspect("equal")
+plt.savefig("comparitive_difference.png")
 '''
